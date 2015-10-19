59022db0b66dd2e8f01f65a5f47ec41e60dc014f
Régis Hanol <regis@hanol.fr>
2015-10-19 20:41:02 +0200

    first commit

---
+++ assets/javascripts/affiliate_dialect.js
@@ -0,0 +1,26 @@
+(function() {
+
+  var AMAZON_DOMAIN_EXTRACTOR_REGEX = /amazon\.([^\?\/\b]{2,})/i;
+  var AMAZON_ASIN_EXTRACTOR_REGEX = /\/([A-Z0-9]{10})[\?\/\b]/i;
+
+  Discourse.Dialect.on("parseNode", function(event) {
+    if (Discourse.SiteSettings.affiliate_enabled) {
+      var node = event.node;
+      if (node[0] === "a") {
+        var href = node[1].href;
+        if (AMAZON_DOMAIN_EXTRACTOR_REGEX.test(href)) {
+          var domain = AMAZON_DOMAIN_EXTRACTOR_REGEX.exec(href)[1];
+          if (AMAZON_ASIN_EXTRACTOR_REGEX.test(href)) {
+            var asin = AMAZON_ASIN_EXTRACTOR_REGEX.exec(href)[1];
+            href = "//www.amazon." + domain + "/dp/" + asin;
+            if (Discourse.SiteSettings.affiliate_amazon_tag.length > 0) {
+              href += "?tag=" + Discourse.SiteSettings.affiliate_amazon_tag;
+            }
+            node[1].href = href;
+          }
+        }
+      }
+    }
+  });
+
+})();
---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-affiliate
+# about: Official affiliation plugin for Discourse
+# version: 0.1
+# authors: Régis Hanol (zogstrip)
+# url: https://github.com/discourse/discourse-affiliate
+
+register_asset "javascripts/affiliate_dialect.js", :server_side
