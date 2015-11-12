5667359de7f45bc99fcdc2259f2c285c962b5275
Régis Hanol <regis@hanol.fr>
2015-11-12 16:43:10 +0100

    tell Discourse which setting enables/disables the plugin

--- plugin.rb
+++ plugin.rb
@@ -4,4 +4,6 @@
 # authors: Régis Hanol (zogstrip)
 # url: https://github.com/discourse/discourse-affiliate
 
+enabled_site_setting :affiliate_enabled
+
 register_asset "javascripts/affiliate_dialect.js", :server_side
