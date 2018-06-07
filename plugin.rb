c596702ff985a4dfa30faadc1514f3b62b70af55
Guo Xiang Tan <tgx_world@hotmail.com>
2018-06-07 17:11:25 +0800

    Don't blow up if database hasn't been created.

--- plugin.rb
+++ plugin.rb
@@ -18,9 +18,13 @@ after_initialize do
   end
 
   # rename "affiliate_amazon_tag" site setting to "affiliate_amazon_com"
-  if SiteSetting.where(name: "affiliate_amazon_tag").exists?
-    SiteSetting.exec_sql("UPDATE site_settings SET name = 'affiliate_amazon_com' WHERE name = 'affiliate_amazon_tag'")
-    SiteSetting.refresh!
+  begin
+    if SiteSetting.where(name: "affiliate_amazon_tag").exists?
+      SiteSetting.exec_sql("UPDATE site_settings SET name = 'affiliate_amazon_com' WHERE name = 'affiliate_amazon_tag'")
+      SiteSetting.refresh!
+    end
+  rescue ActiveRecord::NoDatabaseError
+    nil
   end
 
 end
