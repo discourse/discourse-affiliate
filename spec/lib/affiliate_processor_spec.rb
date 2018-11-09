256ed48aecb3fbd9e35efc30904aae73cccbabb0
Sam <sam.saffron@gmail.com>
2018-11-09 13:03:05 +1100

    clarify we also amend tag

--- spec/lib/affiliate_processor_spec.rb
+++ spec/lib/affiliate_processor_spec.rb
@@ -20,6 +20,7 @@ describe AffiliateProcessor do
     expect(r('https://www.amazon.com')).to eq('https://www.amazon.com?tag=sams-shop')
     expect(r('http://www.amazon.com/some_product?xyz=1')).to eq('http://www.amazon.com/some_product?tag=sams-shop')
     expect(r('https://www.amazon.com/some_product?xyz=1')).to eq('https://www.amazon.com/some_product?tag=sams-shop')
+    expect(r('https://www.amazon.com?hello=1&tag=bobs-shop')).to eq('https://www.amazon.com?tag=sams-shop')
     expect(r('https://amzn.com/some_product?xyz=1')).to eq('https://amzn.com/some_product?tag=sams-shop')
     expect(r('https://smile.amazon.com/some_product?xyz=1')).to eq('https://smile.amazon.com/some_product?tag=sams-shop')
     expect(r('https://www.amazon.com.au/some_product?xyz=1')).to eq('https://www.amazon.com.au/some_product?tag=au-sams-shop')
