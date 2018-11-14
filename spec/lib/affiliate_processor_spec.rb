6367d051e39a3dad650f7c288746f03b78c3a1a1
Rishabh <rishabhnambiar@users.noreply.github.com>
2018-11-14 16:36:53 +0530

    Add spec for a realistic Amazon link

--- spec/lib/affiliate_processor_spec.rb
+++ spec/lib/affiliate_processor_spec.rb
@@ -13,8 +13,9 @@ describe AffiliateProcessor do
     expect(r('https://ldlc.com/some_product?xyz=1')).to eq('https://ldlc.com/some_product?xyz=1#samsshop')
   end
 
-  it 'can apply affiliate code correctly' do
+  it 'can apply affiliate code correctly to amazon' do
     SiteSetting.affiliate_amazon_com = 'sams-shop'
+    SiteSetting.affiliate_amazon_ca = 'ca-sams-shop'
     SiteSetting.affiliate_amazon_com_au = 'au-sams-shop'
 
     expect(r('https://www.amazon.com')).to eq('https://www.amazon.com?tag=sams-shop')
@@ -24,6 +25,7 @@ describe AffiliateProcessor do
     expect(r('https://amzn.com/some_product?xyz=1')).to eq('https://amzn.com/some_product?tag=sams-shop')
     expect(r('https://smile.amazon.com/some_product?xyz=1')).to eq('https://smile.amazon.com/some_product?tag=sams-shop')
     expect(r('https://www.amazon.com.au/some_product?xyz=1')).to eq('https://www.amazon.com.au/some_product?tag=au-sams-shop')
+    expect(r('https://www.amazon.ca/Dragon-Quest-Echoes-Elusive-Age-PlayStation/dp/B07BP3J6RG/ref=br_asw_pdt-5?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=XFGPRSG0SVD5K3RKX5T3&pf_rd_t=36701&pf_rd_p=f8585743-c043-4665-80a7-0cc5fe97d596&pf_rd_i=desktop&th=1')).to eq('https://www.amazon.ca/Dragon-Quest-Echoes-Elusive-Age-PlayStation/dp/B07BP3J6RG/ref=br_asw_pdt-5?tag=ca-sams-shop')
 
     # keep node (BrowseNodeSearch) query parameter
     expect(r('https://www.amazon.com/b?ie=UTF8&node=13548845011')).to eq('https://www.amazon.com/b?tag=sams-shop&node=13548845011')
