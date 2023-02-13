f64a34146aa364b18e424eb356ad514bc6ab3419
Bianca Nenciu <nenciu.bianca@gmail.com>
2023-02-13 17:54:00 +0200

    DEV: Use plugin API to register event handlers

--- plugin.rb
+++ plugin.rb
@@ -11,24 +11,8 @@ enabled_site_setting :affiliate_enabled
 after_initialize do
   require File.expand_path(File.dirname(__FILE__) + "/lib/affiliate_processor")
 
-  DiscourseEvent.on(:post_process_cooked) do |doc, post|
+  on(:post_process_cooked) do |doc, post|
     doc.css("a[href]").each { |a| a["href"] = AffiliateProcessor.apply(a["href"]) }
     true
-  end
-
-  skip_db = defined?(GlobalSetting.skip_db?) && GlobalSetting.skip_db?
-
-  # rename "affiliate_amazon_tag" site setting to "affiliate_amazon_com"
-  if !skip_db
-    begin
-      if SiteSetting.where(name: "affiliate_amazon_tag").exists?
-        SiteSetting.exec_sql(
-          "UPDATE site_settings SET name = 'affiliate_amazon_com' WHERE name = 'affiliate_amazon_tag'",
-        )
-        SiteSetting.refresh!
-      end
-    rescue ActiveRecord::NoDatabaseError
-      nil
-    end
   end
 end
