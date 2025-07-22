d19e6653aaeddaa3ccb57f39c37ce47fa30c2be4
David Taylor <david@taylorhq.com>
2025-07-22 10:42:37 +0100

    implement & spec

--- db/migrate/20250721192553_enable_affiliate_if_already_installed.rb
+++ db/migrate/20250721192553_enable_affiliate_if_already_installed.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 class EnableAffiliateIfAlreadyInstalled < ActiveRecord::Migration[7.2]
-  def up
-    # TODO: check if any of those is !== ""
-    # affiliate_amazon_ca
-    # affiliate_amazon_cn
-    # affiliate_amazon_co_jp
-    # affiliate_amazon_co_uk
-    # affiliate_amazon_com
-    # affiliate_amazon_com_au
-    # affiliate_amazon_com_br
-    # affiliate_amazon_com_mx
-    # affiliate_amazon_de
-    # affiliate_amazon_es
-    # affiliate_amazon_fr
-    # affiliate_amazon_in
-    # affiliate_amazon_it
-    # affiliate_amazon_nl
-    # affiliate_amazon_eu
-    # affiliate_ldlc_com
+  CONFIG_SETTINGS = %w[
+    affiliate_amazon_ca
+    affiliate_amazon_cn
+    affiliate_amazon_co_jp
+    affiliate_amazon_co_uk
+    affiliate_amazon_com
+    affiliate_amazon_com_au
+    affiliate_amazon_com_br
+    affiliate_amazon_com_mx
+    affiliate_amazon_de
+    affiliate_amazon_es
+    affiliate_amazon_fr
+    affiliate_amazon_in
+    affiliate_amazon_it
+    affiliate_amazon_nl
+    affiliate_amazon_eu
+    affiliate_ldlc_com
+  ].freeze
 
-    is_configured = DB.query_single(<<~SQL)&.first
-      SELECT ???
+  def up
+    p is_configured = DB.query_single(<<~SQL, CONFIG_SETTINGS)&.first
+      SELECT 1 FROM site_settings
+      WHERE name IN (?)
+      AND value != ''
+      LIMIT 1
     SQL
 
     if is_configured
